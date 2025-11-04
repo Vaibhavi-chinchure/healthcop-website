45b0d5fa26d9cfff4f76a0447f6ca69d88a6af83
Vaibhavi-chinchure <85163791+Vaibhavi-chinchure@users.noreply.github.com>
2025-11-04 13:49:12 +0530

    Update api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,1 +1,16 @@
+// src/api.js
+import axios from "axios";
 
+const api = axios.create({
+  baseURL: "https://healthcop-website-backend-1.onrender.com/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
